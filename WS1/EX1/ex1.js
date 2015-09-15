909d7ad14fadc2cef7bcee93ac76da4d90eeb65a
BONICHON Nicolas <nicolas.bonichon@devinci.fr>
2015-09-15 15:51:45 +0200

    15:48-push EX1 etEX2 (tout est dans le dossier EX1 pour le moment)

---
+++ WS1/EX1/ex1.js
@@ -0,0 +1,104 @@
+
+'use strict'
+var rental={
+  "cars": [
+    {
+      "id": "p306",
+      "vehicule": "peugeot 306",
+      "pricePerDay": 20,
+      "pricePerKm": 0.10
+    },
+    {
+      "id": "rr-sport",
+      "pricePerDay": 60,
+      "pricePerKm": 0.30
+    },
+    {
+      "id": "p-boxster",
+      "pricePerDay": 100,
+      "pricePerKm": 0.45
+    }
+  ],
+  "rentals": [
+    {
+      "id": "1-pb-92",
+      "driver": {
+        "firstName": "Paul",
+        "lastName": "Bismuth"
+      },
+      "carId": "p306",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-14",
+      "distance": 150
+    },
+    {
+      "id": "2-rs-92",
+      "driver": {
+        "firstName": "Rebecca",
+        "lastName": "Solanas"
+      },
+      "carId": "rr-sport",
+      "pickupDate": "2015-09-09",
+      "returnDate": "2015-09-13",
+      "distance": 550
+    },
+    {
+      "id": "3-sa-92",
+      "driver": {
+        "firstName": " Sami",
+        "lastName": "Ameziane"
+      },
+      "carId": "p-boxster",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-14",
+      "distance": 100
+    }
+  ]
+};
+var liste ={
+"rentals": [
+{
+"id": "1-pb-92",
+
+	  
+},
+{
+"id": "2-rs-92",
+
+},
+{
+"id": "3-sa-92",
+
+}
+]
+};
+
+document.write("<br/> rentals: <br/>");
+for(var k=0;k<=2;k++){
+	document.write(liste.rentals[k].id +"<br/>  Price:" + prix(liste.rentals[k])+ "<br/>");
+}
+
+
+
+
+
+function prix(obj){
+	for(var i=0;i<=2; i++){
+		if(rental.rentals[i].id==obj.id)
+		{
+			for(var j=0;j<=2; j++){
+				if(rental.cars[j].id==rental.rentals[i].carId)
+				{
+					
+					var date= Date.parse(rental.rentals[i].returnDate)- Date.parse(rental.rentals[i].pickupDate);
+					var duree=date/1000/60/60/24+1;
+					
+					var PriceTemp = rental.cars[j].pricePerDay*duree;
+					var PriceKm = rental.rentals[i].distance*rental.cars[j].pricePerKm;
+					var Price= PriceTemp + PriceKm;
+				}
+			}
+		}
+	}
+	return Price;
+}
---
+++ WS1/EX1/ex2.js
@@ -0,0 +1,121 @@
+
+'use strict'
+var rental={
+  "cars": [
+    {
+      "id": "p306",
+      "vehicule": "peugeot 306",
+      "pricePerDay": 20,
+      "pricePerKm": 0.10
+    },
+    {
+      "id": "rr-sport",
+      "pricePerDay": 60,
+      "pricePerKm": 0.30
+    },
+    {
+      "id": "p-boxster",
+      "pricePerDay": 100,
+      "pricePerKm": 0.45
+    }
+  ],
+  "rentals": [
+    {
+      "id": "1-pb-92",
+      "driver": {
+        "firstName": "Paul",
+        "lastName": "Bismuth"
+      },
+      "carId": "p306",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-14",
+      "distance": 150
+    },
+    {
+      "id": "2-rs-92",
+      "driver": {
+        "firstName": "Rebecca",
+        "lastName": "Solanas"
+      },
+      "carId": "rr-sport",
+      "pickupDate": "2015-09-09",
+      "returnDate": "2015-09-13",
+      "distance": 550
+    },
+    {
+      "id": "3-sa-92",
+      "driver": {
+        "firstName": " Sami",
+        "lastName": "Ameziane"
+      },
+      "carId": "p-boxster",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-14",
+      "distance": 100
+    }
+  ]
+};
+var liste ={
+"rentals": [
+{
+"id": "Paul-Bismuth",
+	  
+},
+{
+"id": "Rebecca-Solanas",
+},
+{
+"id": "Sami-Ameziane",
+
+}
+]
+};
+
+document.write("<br/> rentals with decrasing price: <br/>");
+for(var k=0;k<=2;k++){
+	document.write(liste.rentals[k].id +"<br/>  Price:" + prixWithDecrasing(liste.rentals[k])+ "<br/>");
+}
+
+
+
+
+
+function prixWithDecrasing(obj){
+	
+	for(var i=0;i<=2; i++){
+		var drive = rental.rentals[i].driver.firstName +"-"+rental.rentals[i].driver.lastName;
+		if(drive==obj.id)
+		{
+			for(var j=0;j<=2; j++){
+				if(rental.cars[j].id==rental.rentals[i].carId)
+				{
+					
+					var date= Date.parse(rental.rentals[i].returnDate)- Date.parse(rental.rentals[i].pickupDate);
+					var Day=date/1000/60/60/24+1;
+					var PriceTemp = rental.cars[j].pricePerDay*Day;
+					var PriceKm = rental.rentals[i].distance*rental.cars[j].pricePerKm;
+					var Price= PriceTemp + PriceKm;
+					if(Day<1)
+					{
+						var pay= Price;
+					}
+					else if(Day<4)
+					{
+						var pay= Price-(Price*10/100);
+					}
+					else if(Day<10)
+					{
+						var pay= Price-(Price*30/100);
+					}
+					else
+					{
+						var pay= Price-(Price*50/100);
+					}
+				}
+			}
+		}
+	}
+	var price=pay;
+	
+	return price;
+}
