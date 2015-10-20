a73a079ad46ff02f1d3dc686707eb657c3b0d63c
BONICHON Nicolas <nicolas.bonichon@devinci.fr>
2015-10-20 15:52:10 +0200

    derniere version pour WS2 avec un scrapper qui marche!!!!!

--- WS2/Final/server.js
+++ WS2/Final/server.js
@@ -1,24 +1,54 @@
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
 
-var server = http.createServer(function(req, res) {
-    var params = querystring.parse(url.parse(req.url).query);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    if (page == '/home') {
-		document.write(index.html);
-    }
-    else if (page == '/request') {
-        if ('url' in params) {
-			res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+
+//Scrapper LeBonCoin
+app.get('/GETLBC', function(req, res){
+	url = 'http://www.leboncoin.fr/voitures/870647900.htm?ca=12_s';
+
+	
+	request(url, function (error, response, html) {
+		if (!error && response.statusCode == 200) {
+			// lecture page LBC
+			var aRecup='utag_data';
+				var recupEnd='}';
+			var indexVar = html.indexOf(aRecup);
+			var firstCut = html.substr(indexVar+12);
+			var index2Var = firstCut.indexOf(recupEnd);
+			var utag = firstCut.substr(0,index2Var+1);
+	}
+        res.send('Check your console!'+utag_data. marque+'    '+utag_data. modele+'     '+utag_data. annee)
+	})
+})
+
+
+//Send LaCentrale
+app.get('/GETLC', function(req, res){
+	url = 'http://www.leboncoin.fr/voitures/870647900.htm?ca=12_s';
+
+	
+	request(url, function (error, response, html) {
+		if (!error && response.statusCode == 200) {
+			// en passant par un fichier
+				fs.writeFile('output.html', html, function(err){
+				console.log('Scrapping sucess');
+				// lecture page LBC
+			var aRecup='utag_data';
+				var recupEnd='}';
+			var indexVar = html.indexOf(aRecup);
+			var firstCut = html.substr(indexVar+12);
+			var index2Var = firstCut.indexOf(recupEnd);
+			var lastCut = firstCut.substr(0,index2Var+1);
+			fs.writeFile('utag_data.json', lastCut, function(err){
+			})
+        })
 		}
-		else {
-			res.write('Err no url found');
-		}
-    }
-	
-	
-	
-    res.end();
-});
-server.listen(8080);
+        res.send('Check your console!')
+	})
+})
+app.listen('8080')
+console.log('listen 8080');
+exports = module.exports = app; 	
